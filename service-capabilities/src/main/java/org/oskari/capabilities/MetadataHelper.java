f506daacf21cdcd87d7d742b1fd8c35607658ba1
Sami Mäkinen <sami.makinen@nls.fi>
2025-12-08 15:19:22 +0200

    Use URI to decode curly braces

--- service-capabilities/src/main/java/org/oskari/capabilities/MetadataHelper.java
+++ service-capabilities/src/main/java/org/oskari/capabilities/MetadataHelper.java
@@ -15,7 +15,8 @@ import java.util.regex.Pattern;
 
 public class MetadataHelper {
     private static final Logger LOG = LogFactory.getLogger(MetadataHelper.class);
-    private static final Pattern HEX_UPPER_GROUPED = Pattern.compile("^[0-9A-Fa-f]+(?:-[0-9A-Fa-f]+)*$");
+    // dash separated hex char sequences
+    private static final Pattern METADATA_ID_VALIDATOR = Pattern.compile("^[0-9A-Fa-f]+(?:-[0-9A-Fa-f]+)*$");
     /**
      * Helper for parsing metadata uuid from url.
      * @param url
@@ -62,13 +63,18 @@ public class MetadataHelper {
         if (url == null || !url.startsWith("http") || url.length() < 11) {
             return null;
         }
-        // remove possible protocol, we don't care if part of the domain is removed as well
-        String[] pathParts = url.substring(10).split("/");
-
-        for (String possibleId : pathParts) {
-            if(couldBeMetadataId(possibleId)) {
-                return possibleId;
+        // Parse with URI and getPath() to remove params etc
+        // the url-encoded curly braces will be decoded and saved to db as decoded
+        try {
+            String[] pathParts = new URI(url).getPath().split("/");
+            for (String possibleId : pathParts) {
+                if(couldBeMetadataId(possibleId)) {
+                    return possibleId;
+                }
             }
+
+        } catch (Exception e) {
+            LOG.debug("Unexpected error converting url-string to uri:", e);
         }
 
        return null;
@@ -79,11 +85,12 @@ public class MetadataHelper {
             // usually 30+ chars
             return false;
         }
-        if (possibleId.startsWith("%7B") && possibleId.endsWith("%7D")) {
-            possibleId = possibleId.substring(3, possibleId.length() - 3);
+        // URI decodes any URL-encoded bits so we can assume decoded chars here
+        if (possibleId.startsWith("{") && possibleId.endsWith("}")) {
+            possibleId = possibleId.substring(1, possibleId.length() - 1);
         }
 
-        return HEX_UPPER_GROUPED.matcher(possibleId).matches();
+        return METADATA_ID_VALIDATOR.matcher(possibleId).matches();
     }
 
     public static ArrayList<String> getAllowedDomainsList()  {
